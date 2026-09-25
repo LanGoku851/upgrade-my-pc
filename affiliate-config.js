7cf7cdee7c531e04dcac3b5e1195cae6fa3f8482
LanGoku851 <koko85500@gmail.com>
2026-09-25 19:51:13 +0200

    Ajoute la configuration d'affiliation

---
+++ affiliate-config.js
@@ -0,0 +1,16 @@
+/*
+  CONFIGURATION AFFILIATION
+  -------------------------
+  Ne colle ici que les liens fournis par un programme d'affiliation auquel tu as été accepté.
+  Tant qu'un champ reste vide, le site affiche un bouton "Comparer bientôt" au lieu d'envoyer
+  les visiteurs vers un faux lien affilié.
+*/
+window.AFFILIATE_LINKS = {
+  ram16: "",
+  ram32: "",
+  nvme: "",
+  gpuMid: "",
+  gpuHigh: "",
+  cpu: "",
+  monitor: ""
+};
