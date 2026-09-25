41d61f7aa6921dbab1695cf2c12a460c9b7f77e7
LanGoku851 <koko85500@gmail.com>
2026-09-26 01:05:05 +0200

    Ajoute le lien affilié eBay PowerColor RX 9070

--- epn-links.js
+++ epn-links.js
@@ -79,7 +79,7 @@
     'powercolor-reaper-rx9070': {
       customId: 'umpgpureaper9070',
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=PowerColor+Reaper+Radeon+RX+9070+16GB',
-      url: ''
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=PowerColor+Reaper+Radeon+RX+9070+16GB&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214367&customid=umpgpureaper9070&toolid=10001&mkevt=1'
     },
     'powercolor-reaper-rx9070xt': {
       customId: 'umpgpureaper9070xt',
