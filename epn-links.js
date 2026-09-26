3f94ebed40fc037b4515d6656c4657051ac3fda3
LanGoku851 <koko85500@gmail.com>
2026-09-26 16:20:35 +0200

    Ajoute le lien affilié P3 Plus 4 To capacité

--- epn-links.js
+++ epn-links.js
@@ -70,6 +70,11 @@
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=Crucial+P3+Plus+2TB',
       url: 'https://www.ebay.fr/sch/i.html?_nkw=Crucial+P3+Plus+2TB&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214616&customid=umpp3plus2tbcapacity&toolid=10001&mkevt=1'
     },
+    'crucial-p3plus-4to-capacity': {
+      customId: 'umpp3plus4tbcapacity',
+      destination: 'https://www.ebay.fr/sch/i.html?_nkw=Crucial+P3+Plus+4TB',
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=Crucial+P3+Plus+4TB&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214616&customid=umpp3plus4tbcapacity&toolid=10001&mkevt=1'
+    },
 
     'asus-dual-rtx5060-oc8': {
       customId: 'umpgpuasus50608gb',
