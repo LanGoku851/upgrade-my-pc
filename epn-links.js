1a728582ba7ba672bc9458f56dea0df69be4a7cf
LanGoku851 <koko85500@gmail.com>
2026-09-26 14:20:49 +0200

    Ajoute le lien affilié eBay Core Ultra 7 265K

--- epn-links.js
+++ epn-links.js
@@ -110,7 +110,7 @@
     'coreultra7-265k': {
       customId: 'umpcpuultra7265k',
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=Intel+Core+Ultra+7+265K',
-      url: ''
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=Intel+Core+Ultra+7+265K&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214600&customid=umpcpuultra7265k&toolid=10001&mkevt=1'
     },
     'ryzen5700x3d': {
       customId: 'umpcpu5700x3d',
