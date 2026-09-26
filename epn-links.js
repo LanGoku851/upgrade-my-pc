3f82056ff990388e18b58e14253f86e3a1cb84e2
LanGoku851 <koko85500@gmail.com>
2026-09-26 14:38:43 +0200

    Ajoute le lien affilié eBay alimentation 850W Gold

--- epn-links.js
+++ epn-links.js
@@ -151,7 +151,7 @@
     'psu-850-gold': {
       customId: 'umppsu850gold',
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=850W+80+Gold+ATX+3.1+power+supply',
-      url: ''
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=850W+80+Gold+ATX+3.1+power+supply&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214608&customid=umppsu850gold&toolid=10001&mkevt=1'
     },
     'monitor-1440p-180': {
       customId: 'umpmonitor1440p180',
