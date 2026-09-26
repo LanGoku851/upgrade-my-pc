0ecfc9692d92dc1138d9ece70c2025d7070a4dec
LanGoku851 <koko85500@gmail.com>
2026-09-26 14:22:45 +0200

    Ajoute le lien affilié eBay Ryzen 7 5700X3D

--- epn-links.js
+++ epn-links.js
@@ -115,7 +115,7 @@
     'ryzen5700x3d': {
       customId: 'umpcpu5700x3d',
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=AMD+Ryzen+7+5700X3D',
-      url: ''
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=AMD+Ryzen+7+5700X3D&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214600&customid=umpcpu5700x3d&toolid=10001&mkevt=1'
     },
     'ryzen5800x3d': {
       customId: 'umpcpu5800x3d',
