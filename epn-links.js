4ac0fa2e131196c0366c007a2e1d02d1e3f3b199
LanGoku851 <koko85500@gmail.com>
2026-09-26 14:48:24 +0200

    Ajoute le lien affilié eBay RAM 32 Go DDR4

--- epn-links.js
+++ epn-links.js
@@ -27,7 +27,7 @@
     'ram32-ddr4': {
       customId: 'umpram32ddr4',
       destination: 'https://www.ebay.fr/sch/i.html?_nkw=32GB+DDR4+3200+CL16+kit',
-      url: ''
+      url: 'https://www.ebay.fr/sch/i.html?_nkw=32GB+DDR4+3200+CL16+kit&mkcid=1&mkrid=709-53476-19255-0&siteid=71&campid=5339214611&customid=umpram32ddr4&toolid=10001&mkevt=1'
     },
     'ram32-ddr5': {
       customId: 'umpram32ddr5',
